4910c8ff62de054d0b8215a7b98e87e7a0cf1972
IDEATEKNOLOJI\ahmet.basol <ahmetefe.basol@ogr.iuc.edu.tr>
2022-07-20 15:07:48 +0300

    meh

--- main.py
+++ main.py
@@ -1,50 +1,42 @@
 # %% import
 from bs4 import BeautifulSoup
 import requests
+from urllib.request import urlopen
 import pandas as pd
-import openpyxl
+
+f = open("Car_Info.tsv", "w")
 
 # %% HTTP request (store website in variable)
 arabamCom_CarSales = "https://www.arabam.com/ikinci-el/otomobil/fiat?take=50&page=1"
 arabamCom_website = "https://www.arabam.com"
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
 
-# %% HTTP REQUEST (get request)
-response = requests.get(arabamCom_CarSales, headers=headers)
+# %% Assign car adverts links to set
+wantedlinks = []
 
-# %% HTTP REQUEST (status code) need 200 not 429
-response.status_code
+for i in range(1, 51, 1):
+    response = requests.get(arabamCom_CarSales,
+                            headers=headers)  # https://www.arabam.com/ikinci-el/otomobil/fiat?take=50&page=1
+    soup = BeautifulSoup(response.content, "html.parser")
+    results = soup.find_all("tr", class_="listing-list-item")
+    for result in results:
+        href_tags = result.find("a")["href"]
+        birlesim = arabamCom_website + href_tags
+        wantedlinks.append(birlesim)
 
-# %% SOUP OBJECT
-soup = BeautifulSoup(response.content, "html.parser")
-soup
-
-# %% RESULTS
-results = soup.find_all("tr", class_="listing-list-item")
-print(len(results))
-links = []
-
-for result in results:
-    href_tags = result.find("a")["href"]
-    birlesim = arabamCom_website + href_tags
-    print(birlesim)
-    links.append(birlesim)
-
-print(links)
-
-#%% WRITE TXT
-with open(r'car_links.txt', 'w') as fp:
-    for item in links:
-        # write each item on a new line
-        fp.write("%s\n" % item)
-    print('Done')
+car_dealer = pd.DataFrame({'links':wantedlinks})
+print(car_dealer)
 
 # %% GET INSIDE LINK
-response_inside = requests.get(birlesim, headers=headers)
-print(response_inside.status_code)
+detailedCodes = []
 
-soup_inside = BeautifulSoup(response_inside.content, "html.parser")
-soup_inside
+for i in wantedlinks:
+    response_inside = requests.get(str(i), headers=headers)
+    print(response_inside.status_code)
+    soup_inside = BeautifulSoup(response_inside.content, features="html.parser")
+    detailedCodes.append(soup_inside)
+    print(i)
 
 # %% INSIDE RESULTS
 results_inside = soup_inside.find_all("div", class_="banner-column-detail bcd-mid-extended p10 bg-white")
@@ -53,7 +45,7 @@ print(len(results_inside))
 # %% GET VARIABLES
 car_price = results_inside[0].find("div", {"class": "df df-center w50"}).get_text().strip()
 car_location = results_inside[0].find("p", {
-"class": "one-line-overflow font-default-minus pt4 color-black2018 bold"}).get_text().strip()
+    "class": "one-line-overflow font-default-minus pt4 color-black2018 bold"}).get_text().strip()
 
 car_list = []
 data1 = results_inside[0].find('ul')
